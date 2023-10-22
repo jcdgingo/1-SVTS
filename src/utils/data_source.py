c0750a5361dfba26a71fb4da111f25e3dbc713a6
Jess <jcdgingo@gmail.com>
2023-10-22 17:59:52 -0400

    corrected code

--- src/utils/data_source.py
+++ src/utils/data_source.py
@@ -11,30 +11,26 @@ class FinancialAnalytics:
         self.date_from = date_from
         self.file_path = file_path
         self.sort = sort
-# fetch_market_data method has a raw data in TSV file and the code is created to convert it to csv
+
     def fetch_market_price_data(self):
-        file_path = self.file_path       
-        df_tsv = pd.read_csv(file_path, sep='\t')
-        df_tsv.drop('<VOL>', axis=1, inplace=True)# <VOL> is dropped since FOREX has no real volume, only ticker volume
+        df_tsv = pd.read_csv(self.file_path, sep='\t')
+        df_tsv.drop('<VOL>', axis=1, inplace=True)
         df_tsv['DateTime'] = pd.to_datetime(df_tsv['<DATE>'] + ' ' + df_tsv['<TIME>'])
         df_tsv.drop(['<DATE>', '<TIME>'], axis=1, inplace=True)
         df_tsv = df_tsv[['DateTime', '<OPEN>', '<HIGH>', '<LOW>', '<CLOSE>', '<TICKVOL>', '<SPREAD>']]
         output_csv_file_path = r'C:\Users\jessi\OneDrive\1 Projects\1-svts\data\interim\EURUSD_M1.csv'
         df_tsv.to_csv(output_csv_file_path, index=False)
         return df_tsv
-# fetch market news sentiment from Alpha Vantage API, code for cleaning from nested json format and extract sentiment
+
     def fetch_news_sentiment_data(self, apikey='151J1CO4W4YLKP87'):
-        ticker = self.ticker
-        date_from = self.date_from
-        sort = self.sort    
-        url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&time_from={date_from}&limit=1000&sort={sort}&apikey={apikey}'
+        url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={self.ticker}&time_from={self.date_from}&limit=1000&sort={self.sort}&apikey={apikey}'
         r = requests.get(url)
         eur_news_data = r.json()
         df = pd.DataFrame(eur_news_data)
         nested_df = pd.json_normalize(df['feed'])
-        filtered_data = nested_df['ticker_sentiment'].apply(lambda x: any(item.get('ticker') == ticker for item in x))
+        filtered_data = nested_df['ticker_sentiment'].apply(lambda x: any(item.get('ticker') == self.ticker for item in x))
         nested_df = nested_df[filtered_data]
-        nested_df['ticker_sentiment'] = nested_df['ticker_sentiment'].apply(lambda x: [item for item in x if item.get('ticker') == ticker][0])
+        nested_df['ticker_sentiment'] = nested_df['ticker_sentiment'].apply(lambda x: [item for item in x if item.get('ticker') == self.ticker][0])
         eur_sentiment = pd.json_normalize(nested_df['ticker_sentiment'])
         df_time = pd.json_normalize(df['feed'])
         df_time['time_published'] = pd.to_datetime(df_time['time_published'], format='%Y%m%dT%H%M%S')
@@ -42,9 +38,8 @@ class FinancialAnalytics:
         eur_sentiment['time_published(est)'] = df_time['time_published']
         return eur_sentiment
 
-    def cluster_data(self):
-        file_path = self.file_path 
-        df_cluster = file_path.fetch_market_price_data()
+    def cluster_data(self): 
+        df_cluster = self.fetch_market_price_data()
         scaler = StandardScaler()
         scaled_df = scaler.fit_transform(df_cluster)
         wcss = []
@@ -57,7 +52,7 @@ class FinancialAnalytics:
         plt.xlabel('Number of clusters')
         plt.ylabel('WCSS')
         plt.show()
-        best_k = 6  # Adjust this based on your observation from the elbow graph
+        best_k = 6  # Assuming 6 is used based on your domain knowledge
         kmeans = KMeans(n_clusters=best_k, init='k-means++', max_iter=300, n_init=10, random_state=0)
         pred_y = kmeans.fit_predict(scaled_df)
         df_cluster['Cluster'] = pred_y
